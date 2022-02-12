8c48eaa9f44dc25d13a80be158f5deb17371ea9e
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-02-12 17:45:42 +0700

    header done

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Image from 'next/image';
-import {SearchIcon} from '@heroicons/react/solid';
-export default function Header() {
+import {
+    SearchIcon,
+    GlobeAltIcon,
+    MenuIcon,
+    UserCircleIcon,
+    UsersIcon,
+} from '@heroicons/react/solid';
+
+function Header() {
   return (
    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
        {/* Left */}
-       <div className="relative flex items-center h-12 cursor-pointer my-auto">
+       <div className="relative flex items-center h-16 cursor-pointer my-auto">
            <Image  src="/olivelogo.png" 
            layout="fill"
            objectFit="contain"
@@ -14,13 +21,22 @@ export default function Header() {
        </div>
 
        {/* Middle */}
-       <div>
-           <input type="text" placeholder="Search daycare"/>
-           <SearchIcon className="h-8 bg-teal-500 text-white rounded-full p-2 cursor-pointer"/>
+       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+           <input className="flex-grow pl-5 bg-transparent outline-none" type="text" placeholder="ค้นหาศูนย์ดูแล"/>
+           <SearchIcon className="hidden md:inline-flex h-8 bg-teal-500 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
        </div>
 
        {/* Right */}
-       <div></div>
+       <div className="flex items-center space-x-4 justify-end text-gray-500">
+            <p className="hidden md:inline">เป็นเจ้าของศูนย์ดูแล</p>
+            <GlobeAltIcon className="h-6" />
+
+            <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                <MenuIcon className="h-6" />
+                <UserCircleIcon className="h-6" />
+            </div>
+       </div>
    </header>
-  )
+  );
 }
+export default Header;
