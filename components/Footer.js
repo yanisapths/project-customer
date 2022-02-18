92db6eadec4213d02e2d0ea0fd7193c5598b315d
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-02-18 16:25:33 +0700

    nect/link

--- components/Footer.js
+++ components/Footer.js
@@ -5,22 +5,23 @@ import {
     UserGroupIcon,
     CalendarIcon,
 } from '@heroicons/react/solid';
+import Link from "next/link"
 
 function Footer() {
   return (
       <footer className="fixed bottom-0 inset-x-0  bg-white flex justify-between text-teal-900 font-extrabold shadow-black shadow-md md:rounded-t-full rounded-t-2xl">
-                <a href="/" className="a">
+                <Link href="/" className="a">
                 <HomeIcon  className="footer-icon"/>หน้าหลัก
-                </a>
-                <a href="#" className="a">
+                </Link>
+                <Link href="/" className="a">
                 <PlusCircleIcon  className="footer-icon"/>สร้างนัด
-                </a>
-                <a href="/family/" className="a">
+                </Link>
+                <Link href="/family/" className="a">
                 <UserGroupIcon  className="footer-icon"/>ครอบครัว
-                </a>
-                <a href="#" className="a">
+                </Link>
+                <Link href="/" className="a">
                 <CalendarIcon  className="footer-icon"/>ตารางนัด
-                </a>
+                </Link>
       </footer>
    
   )
