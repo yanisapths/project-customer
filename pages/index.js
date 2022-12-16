83316754e0f1e781742ab00c16b3ea2d398c039e
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2022-12-16 20:14:51 +0700

    add call clinics api

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import InfoCard from "../components/InfoCard"
 import { getSession } from "next-auth/react";
 
-export default function Home({ session }) {
+export default function Home({ session, data }) {
   return (
     <div className="">
       <Head>
@@ -16,6 +17,27 @@ export default function Home({ session }) {
       <main className="main h-screen overflow-scroll scrollbar-hide">
         <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3 lg:pt-12"></div>
         <section className=" py-4 flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3 "></section>
+        <div className="flex flex-col">
+          {data?.map(
+            ({
+              clinic_id,
+              clinic_name,
+              imageUrl,
+              address,
+              description,
+              price,
+            }) => (
+              <InfoCard
+                key={clinic_id}
+                clinic_name={clinic_name}
+                imageUrl={imageUrl}
+                address={address}
+                description={description}
+                price={price}
+              />
+            )
+          )}
+        </div>
       </main>
       <Footer />
     </div>
@@ -24,7 +46,10 @@ export default function Home({ session }) {
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+  const data = await fetch(
+    "https://ukbze987lk.execute-api.ap-northeast-1.amazonaws.com/dev/clinic"
+  ).then((res) => res.json());
   return {
-    props: { session },
+    props: { session, data },
   };
 }
