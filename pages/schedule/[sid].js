3526e404a1bf6d2a8a11174cfec233aaaa46515b
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-22 15:39:01 +0700

    update: appointment table

--- pages/schedule/[sid].js
+++ pages/schedule/[sid].js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 import Head from "next/head";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import NavigateBack from "../../components/OLNavigateBack/NavigateBack";
 
-function ScheduleDetail({data}) {
+function ScheduleDetail({ data }) {
+  const { data: session, status } = useSession();
   const [course, setCourse] = useState({});
   const [clinic, setClinic] = useState({});
   const router = useRouter();
+  console.log(data)
 
   const fetchData = async () => {
     let isSubscribed = true;
@@ -29,11 +33,12 @@ function ScheduleDetail({data}) {
   };
 
   useEffect(() => {
-    const courseId = data.course_id;
-    if(courseId) {
+    if (status === "unauthenticated") {
+      router.push("/auth/signin/");
+    } else {
       fetchData().catch(console.error);
     }
-  },);
+  }, [status]);
 
   return (
     <div>
@@ -43,14 +48,16 @@ function ScheduleDetail({data}) {
       </Head>
       <Header />
       <main className="lg:pb-32 h-screen overflow-scroll scrollbar-hide">
-         <NavigateBack path="/schedule" />
+        <NavigateBack path="/schedule" />
         <div className="px-6 pt-12 lg:px-96 text-center">
           <p className="body1 tracking-wide text-black/50">ตารางนัด</p>
           <h2 className="py-6 h3 lg:h1">{course.courseName}</h2>
-          <div className="bg-[#7BC6B7] w-fit px-6 py-2 lg:py-3 rounded-full mx-auto">
-            <p className="h6 text-white">{data.clinicName}</p>
+          <div className="cursor-pointer bg-[#7BC6B7] transition hover:shadow-xl hover:shadow-[#7BC6B7]/40 w-fit px-6 py-2 lg:py-3 rounded-full mx-auto">
+            <Link href={`/clinic/${data.clinic_id}`}>
+              <p className="h6 text-white">{data.clinicName}</p>
+            </Link>
           </div>
-          <div className="flex justify-between body1  lg:h6 tracking-wide mt-6 lg:mt-12 lg:px-96">
+          <div className="flex justify-between body1 lg:h6 tracking-wide mt-6 lg:mt-12 lg:px-96">
             <div>
               <p className=" text-black/50">ติดต่อคลินิก</p>
               <p className=" hover:text-[#0921FF]">{clinic.phoneNumber}</p>
@@ -65,8 +72,11 @@ function ScheduleDetail({data}) {
               <div className="relative block md:w-1/6">
                 <p className="">ครั้งที่</p>
               </div>
-              <div className="relative block md:w-4/6">
-                <p>วันรักษา</p>
+              <div className="relative block md:w-1/6">
+                <p>วันนัด</p>
+              </div>
+              <div className="relative block md:w-1/6">
+                <p>เวลานัด</p>
               </div>
               <div className="relative block md:w-1/6">
                 <p>สถานะ</p>
@@ -74,37 +84,34 @@ function ScheduleDetail({data}) {
             </div>
           </section>
 
-          <div className="flex justify-between p-2 bg-gray-100 text-black/50 mb-1  mx-2 md:ml-8 lg:w-full body1 md:h6 lg:h5">
+          <div className="flex justify-between p-2 bg-[#acded5]/20 text-black mb-1  mx-2 md:ml-8 lg:w-full body1 md:h6 lg:h5">
             <div className="md:w-1/6">
               <p>1</p>
             </div>
-            <div className="md:w-4/6">
-              <p>01/02/2022</p>
+            <div className="md:w-1/6">
+              <p>{new Date(data.appointmentDate).toDateString()}</p>
             </div>
             <div className="md:w-1/6">
-              <p>สำเร็จ</p>
-            </div>
-          </div>
-          <div className="flex justify-between p-2 bg-gray-100 text-black/50 mb-1  mx-2 md:ml-8 lg:w-full body1 md:h6 lg:h5">
-            <div className="md:w-1/6">
-              <p>2</p>
-            </div>
-            <div className="md:w-4/6">
-              <p>10/02/2022</p>
-            </div>
-            <div className="md:w-1/6">
-              <p>สำเร็จ</p>
-            </div>
-          </div>
-          <div className="flex justify-between p-2 bg-[#acded5] mb-1  mx-2 md:ml-8 lg:w-full body1 md:h6 lg:h5">
-            <div className="md:w-1/6">
-              <p>3</p>
-            </div>
-            <div className="md:w-4/6">
-              <p className="cursor-pointer hover:text-[#0921FF]">เลือกวัน</p>
+              <p>
+                {new Date(data.appointmentTime).toLocaleTimeString("en-EN", {
+                  hour: "numeric",
+                  minute: "2-digit",
+                  hour12: true,
+                })}
+                {data.endTime ? 
+                <>
+                 {"-"}{new Date(data.endTime).toLocaleTimeString("en-EN", {
+                  hour: "numeric",
+                  minute: "2-digit",
+                  hour12: true,
+                })}
+                </>
+                :
+                <></>}
+              </p>
             </div>
             <div className="md:w-1/6">
-              <p>ยังไม่เริ่ม</p>
+              <p>{data.status}</p>
             </div>
           </div>
         </div>
@@ -142,4 +149,3 @@ export async function getStaticProps({ params }) {
     props: { data },
   };
 }
-
