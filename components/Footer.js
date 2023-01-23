16e95e98dbd7ad7bfefaf4cd59b936c0721a7bde
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-23 13:55:52 +0700

    style(footer,header): change responsive

--- components/Footer.js
+++ components/Footer.js
@@ -20,12 +20,12 @@ function Footer() {
   ];
 
   return (
-    <footer className="fixed bottom-5 inset-x-0 flex justify-between shadow-black/10 shadow-3xl rounded-full mx-8 xl:mx-96 transition-colors duration-500 dark:bg-[#FDFFF5]">
+    <footer className="fixed bottom-5 inset-x-0 flex justify-between shadow-black/10 shadow-3xl rounded-full mx-8 md:mx-40 lg:mx-60 xl:mx-96 transition-colors duration-500 dark:bg-[#FDFFF5]">
       {list.map((item) => (
         <Link href={item.path}>
           <footer-container>
             <div className="footer-icon">{item.icon}</div>
-            <p className="caption">{item.title}</p>
+            <p className="caption lg:caption xl:body2">{item.title}</p>
           </footer-container>
         </Link>
       ))}
