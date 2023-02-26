ec77ba02eb1dc312f77fe654e3f5dd29cf1d915c
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-02-26 09:39:25 +0700

    add: type

--- components/OLCard/BannerCard.js
+++ components/OLCard/BannerCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTheme } from "@mui/material/styles";
 
-function BannerCard({ courseName, procedures }) {
+function BannerCard({ courseName, procedures, type }) {
   const theme = useTheme();
   const procedureList = procedures ? (
     procedures?.map(({ procedureName }) => procedureName + ",")
@@ -11,7 +11,18 @@ function BannerCard({ courseName, procedures }) {
 
   return (
     <div className="px-4 xl:px-8">
-      <p className="h4 pt-6 p-4 pb-2 font-mitr text-[#005844] truncate">{courseName}</p>
+      <div className="flex pt-4 pb-2">
+      <p className="h4 p-4 font-mitr text-[#005844] truncate">
+        {courseName}
+      </p>
+      {type != "false" ? (
+        <strong className="rounded-full bg-[#A5A6F6]/20 text-[#7879F1] px-2 py-1 text-xs font-medium self-center">
+          {type}
+        </strong>
+      ) : (
+        <></>
+      )}
+      </div>
       {procedures ? (
         <p className="pb-4 px-4 pr-24 tracking-wide body1 text-[#005844] truncate">
           {procedureList}
--- pages/course/[id].js
+++ pages/course/[id].js
@@ -40,9 +40,18 @@ function CourseDetail({ data, course }) {
       <main className="h-screen overflow-scroll scrollbar-hide">
         <div className="px-6 pt-12 xl:px-96">
           <NavigateBack path={`/clinic/${cid}`} />
-          <h2 className="pt-6 h3 lg:h1 text-center text-[#005844]">
+          <div className="flex justify-center gap-2 pt-6">
+          <h2 className="h3 lg:h1 text-[#005844]">
             {course.courseName}
           </h2>
+          {course.type != "false" ? (
+            <strong className="rounded-full bg-[#A5A6F6]/20 text-[#7879F1] px-2 py-1 text-sm font-medium self-center">
+              {course.type}
+            </strong>
+          ) : (
+            <></>
+          )}
+          </div>
           <section className="xl:w-5/6 mb-2 pt-10 md:pt-20 overflow-scroll scroll-auto scrollbar-hide mx-2 md:ml-8 border-black/20  border-b-[1px] border-dashed">
             <div className="flex justify-between lg:body1 tracking-wide  text-black/50">
               <div className="relative block ">
