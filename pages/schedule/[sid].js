629054b03fe0c2c32ffd85fabc86c408c52b6f22
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-03-03 13:34:56 +0700

    fix: ur; event on schedule no event

--- pages/schedule/[sid].js
+++ pages/schedule/[sid].js
@@ -247,7 +247,7 @@ export async function getStaticProps({ params }) {
   const data = await res.json();
   const courseurl = `${process.env.url}/course/${data.course_id}`;
   const clinicurl = `${process.env.url}/clinic/${data.clinic_id}`;
-  const eventurl = `${process.env.url}/event/match/clinic/${data._id}`;
+  const eventurl = `${process.env.url}/event/match/${data._id}`;
   try {
     const courses = await fetch(courseurl);
     const clinics = await fetch(clinicurl);
