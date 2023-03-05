1b4290b977f3ba9d4de777e51c703f9580055024
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-03-05 08:23:36 +0700

    remove onClick on review

--- components/OLCard/ReviewCard.js
+++ components/OLCard/ReviewCard.js
@@ -51,7 +51,6 @@ function ReviewCard({ schedule_id, course_id, clinicName, status, clinic_id }) {
 
   return (
     <Box
-      onClick={() => navigate(status, `/schedule/${schedule_id}`)}
       className="cursor-pointer rounded-2xl shadow-xl overflow-x-auto p-4 pt-8 w-full lg:mb-8 mb-4 min-h-[250px] transition hover:shadow-2xl overflow-hidden"
       sx={{ bgcolor: alpha(theme.palette.iris.light, 0.1), width: "100%" }}
     >
