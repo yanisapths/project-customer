b4fc0d5d91596458718752b5bd429daef0e2131b
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-03-07 12:53:50 +0700

    add: course service default

--- components/OLForm/CourseService.js
+++ components/OLForm/CourseService.js
@@ -41,6 +41,9 @@ export class CourseService extends Component {
             value={values.course_id}
             onChange={handleChange("course_id")}
           >
+            <MenuItem value="ตรวจร่างกาย">
+            ตรวจร่างกาย (แนะนำสำหรับลูกค้าใหม่)
+            </MenuItem>
             {courses?.map((input, key) => (
               <MenuItem
                 key={key}
